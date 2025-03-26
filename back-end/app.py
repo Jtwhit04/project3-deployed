e012fa531603e08dfce2cbc096a21ec41c954cad
JTW <jtwhit04@tamu.edu>
2025-03-26 17:14:36 -0500

    port

--- back-end/app.py
+++ back-end/app.py
@@ -4,6 +4,7 @@ from time import sleep
 from dotenv import load_dotenv
 import os
 
+
 from models.employee import Employee
 from models.menuitem import MenuItem
 
@@ -123,4 +124,4 @@ class API:
             return jsonify(ingredients)
 
     def run(self):
-        self.app.run(port=5000)
+        self.app.run(port = os.getenv("port") or 5000)
